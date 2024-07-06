b11dcd817cc8443671f700ae06408128c424c7e4
Fainche Mckeown <faicatmck@gmail.com>
2024-07-06 13:51:06 +0100

    icon image

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-   
+    let iconElement = document.querySelector("#icon");
     
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
@@ -15,6 +15,8 @@ function refreshWeather(response) {
     temperatureElement.innerHTML = Math.round(temperature); 
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
 }
 
 function formatDate(date) {    
